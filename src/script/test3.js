bfd28ba820cd424a39d61848000d209ad5d0d494
Shakir Thelmue <shakirthelmue@gmail.com>
2023-05-13 22:56:56 -0500

    done for the day

--- src/script/test3.js
+++ src/script/test3.js
@@ -1,8 +1,9 @@
 const https = require('https');
 const fs = require('fs');
 const zlib = require('zlib');
+const csv2json = require("csv2json")
 
-function download(url, inputPath) {
+function download(url, inputPath, callback) {
     const stream = fs.createWriteStream(inputPath)
     https.get(url, (res) => {
         const unzippedStream = res.pipe(zlib.createGunzip())
@@ -11,12 +12,28 @@ function download(url, inputPath) {
 
     stream.on("finish", () => {
         console.log("unzipped files done downloading")
+        callback()
     })
 
     stream.on("error", (error) => {
         console.log(error)
     })
 }
+
+function tsv2json(input, output) {
+    let options = {
+        encoding: "utf-8",
+        highWaterMark: 16
+    }
+    const reader = fs.createReadStream(input, options)
+    const writer = fs.createWriteStream(output, options)
+    reader.pipe(
+        csv2json({separator: "\t"}, { objectMode:true })
+    ).on("data", (chunk) => {console.log(chunk.toString())}).pipe(writer)
+    writer.on('finish', () => {
+      console.log("finished conversion")
+    })
+  }
 
 const datasets = [
     "https://datasets.imdbws.com/name.basics.tsv.gz", 
@@ -32,5 +49,8 @@ for (let i in datasets) {
     let url = datasets[i]
     let name = url.split('/')[3] // splitting the url by "/",choosing the fouth value in the array 
     let path = "/home/risaia/FlickFinder/src/model/imdb_data/" + name.replace(".gz", "")
-    download(url, path)
+    let outputPath = path.replace(".tsv", ".json")
+    download(url, path, () => {
+        tsv2json(path, outputPath)
+    })
 }
